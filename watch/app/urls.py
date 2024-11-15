3df0fbfda5b8b04b57cf0512615cd17c5cdc4ee9
ittirarichan <abhishekbinish86@gmail.com>
2024-11-15 11:34:18 +0530

    product adding

--- watch/app/urls.py
+++ watch/app/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns=[
     path('',views.watch_login),
+    path('register',views.register),
     path('watch_home',views.watch_home),
     path('watch_logout',views.watch_shop_logout),
+    path('add_product',views.add_product),
 ]
--- watch/app/views.py
+++ watch/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from .models import *
 
 
 # Create your views here.
@@ -30,5 +31,29 @@ def watch_shop_logout(req):
 
 def watch_home(req):
     if 'shop' in req.session:              #checking section status
-        return render(req,'watch/home.html')
+        data=Product.objects.all()
+        return render(req,'watch/home.html',{'products':data})
     return render(req,'register.html')
+
+
+def register(req):
+    return render(req,'register.html')
+
+def add_product(req):
+    if 'shop' in req.session:
+        if req.method=='POST':
+            product_id=req.POST['product_id']
+            name=req.POST['name']
+            description=req.POST['description']
+            price=req.POST['price']
+            offer_price=req.POST['offer_price']
+            stock=req.POST['stock']
+            file=req.FILES['image']
+            data=Product.objects.create(pid=product_id,name=name,dis=description,
+                                         price=price,offer_price=offer_price,stock=stock,img=file)
+            data.save()
+            return redirect(watch_home)
+        else:
+            return render(req,'watch/add_product.html')
+    else:
+        return redirect(watch_login)
